1d79efa9dd92c5f97fe4a8c016eccf83c7fe0691
aryabun <bunarya@yahoo.com>
2026-06-03 12:19:31 +0700

    [UPDATE] missing path to BasicFilter in DosageForm model during merging

--- app/Models/DosageForm.php
+++ app/Models/DosageForm.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Traits\BasicFilter;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
